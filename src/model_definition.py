bfef60a704d67c3dfcf99056ff3e5de54bc7f036
Alessandro <alessandro.maggio98@gmail.com>
2021-12-26 04:30:58 +0100

    Modelling Layer, everything fine until now.

--- src/model_definition.py
+++ src/model_definition.py
@@ -3,7 +3,8 @@ import keras
 from keras import layers
 from keras.models import Sequential
 import tensorflow as tf
-from keras.layers import Dense, Multiply
+from keras import backend as K
+from keras.layers import Dense, Input
 from keras.layers import LSTM, Bidirectional
 from keras.models import Model
 
@@ -18,55 +19,53 @@ def word_embbedding_layer(max_seq_length, tokenizer):
     return embedding_layer
 
 
-########  ATTENTION ########
+def build_context_to_query(context_layer, query_layer, n_heads=2, head_dim=4):
+    attention_layer_c2q = tf.keras.layers.MultiHeadAttention(num_heads=n_heads, key_dim=head_dim)
+    return attention_layer_c2q(context_layer, query_layer)  # output shape (context_lenght, Emb_dim)
 
 
-def pairwise_cosine_sim(params):
-    """
-    TO BE FIXED
+def build_query_to_context(context_layer, query_layer, context_lenght, n_heads=2, head_dim=4):
+    attention_layer_q2c = tf.keras.layers.MultiHeadAttention(num_heads=n_heads, key_dim=head_dim)
+    attention_tensor = attention_layer_q2c(query_layer, context_layer)  # output shape (query_lenght, Emb_dim)
+    max_column = tf.nn.softmax(tf.math.reduce_max(attention_tensor, axis=-2))  # compute the most important words in the context wrt the query
+    q2c_tensor = tf.repeat(tf.expand_dims(max_column, -2), context_lenght, axis=-2)    # duplicate the column for every context word
+    return q2c_tensor
 
-    A [batch x n x d] tensor of n rows with d dimensions
-    B [batch x m x d] tensor of m rows with d dimensions
-    returns:
-    D [n x m] tensor of similarity matrix obtained using the trainable weights
-    """
 
-    C, Q, weights = params
-    matrix = []
-    for t in range(C.shape[1]):
-        row = []
-        for j in range(Q.shape[1]):
-            h_u_hdu = tf.concat([C[:, t, :], Q[:, j, :], tf.math.multiply(C[:, t, :], Q[:, j, :])], axis=1)
-            s_tj = tf.reduce_sum(tf.math.multiply(weights, h_u_hdu))
-            row.append(s_tj)
-        matrix.append(tf.stack(row))
-        print(t)
-
-    similarity_matrix = tf.stack(matrix)
-    print("Similarity matrix shape: " + str(similarity_matrix.shape))
-    return similarity_matrix
+def build_overall_attention_tensor(context_tensor, context_to_query_tensor, query_to_context_tensor):
+    context_c2q = tf.math.multiply(context_tensor, context_to_query_tensor)
+    context_q2c = tf.math.multiply(context_tensor, query_to_context_tensor)
+    return tf.concat(values=[context_tensor, context_to_query_tensor, context_c2q, context_q2c], axis=-1)
 
 
 def model_definition(context_max_lenght, query_max_lenght, tokenizer_x):
+
     # initialize two distinct models
-    model_context = Sequential()
-    model_query = Sequential()
+    context_input = Input(shape=(context_max_lenght,))
+    query_input = Input(shape=(query_max_lenght,))
 
     # adding the Embedding (words) layer to both the models
-    model_context.add(word_embbedding_layer(context_max_lenght, tokenizer_x))
-    model_query.add(word_embbedding_layer(query_max_lenght, tokenizer_x))
+    context_embedding = word_embbedding_layer(context_max_lenght, tokenizer_x)(context_input)
+    query_embedding = word_embbedding_layer(query_max_lenght, tokenizer_x)(query_input)
 
     # Contextual Embedding Layer
-    model_context.add(Bidirectional(LSTM(EMBEDDING_DIM, return_sequences=True)))
-    model_query.add(Bidirectional(LSTM(EMBEDDING_DIM, return_sequences=True)))
+    context_contestual_embedding = Bidirectional(LSTM(EMBEDDING_DIM, return_sequences=True))(context_embedding)
+    query_contestual_embedding = Bidirectional(LSTM(EMBEDDING_DIM, return_sequences=True))(query_embedding)
 
     # Attention Flow Layer
-    weights = tf.Variable(initial_value=tf.random.normal((300,)), trainable=True)
-    # weights = Dense(300, activation='linear', use_bias=False)
-    similarity_matrix_layer = keras.layers.Lambda(pairwise_cosine_sim)((model_context.output, model_query.output, weights))
 
-    place_holder = Dense(1)(similarity_matrix_layer)  # FIXME
+    context_to_query_tensor = build_context_to_query(context_contestual_embedding, query_contestual_embedding)
+    query_to_context_tensor = build_query_to_context(query_contestual_embedding, context_contestual_embedding, context_max_lenght)
+    overall_attention_tensor = build_overall_attention_tensor(context_contestual_embedding, context_to_query_tensor, query_to_context_tensor)
+
+    # Modeling Layer
+
+    query_context_contextual_tensor = Bidirectional(LSTM(EMBEDDING_DIM, return_sequences=True))(overall_attention_tensor)
+    query_context_contextual_tensor = Bidirectional(LSTM(EMBEDDING_DIM, return_sequences=True))(query_context_contextual_tensor)
+
+    # END
+    place_holder = Dense(1)(query_context_contextual_tensor)  # FIXME
     #  print
-    model = keras.Model(inputs=[model_context.input, model_query.input], outputs=place_holder)
+    model = keras.Model(inputs=[context_input, query_input], outputs=place_holder)
     model.compile()
     print(model.summary())
